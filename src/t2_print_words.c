995d87dfe6a1a12e61baac5e170ee9747a560130
gudwidfud <timrodionov2002@gmail.com>
2023-10-24 14:50:52 +0100

    commit 3

--- src/t2_print_words.c
+++ src/t2_print_words.c
@@ -8,11 +8,11 @@ void print_words(char input_text[]) {
     char word[50];
     char ch = ' ';
     char nu = '\0'; 
-    if (input_text != '\0'){
-        for (int i = 0; word[i] != '\0';i ++){
-            for (int j = 0; i <= strlen(input_text); i++){
-                word[i] = input_text[j];
-                if (input_text[j] == ch || input_text[j] == nu){
+    
+    for (int i = 0; word[i] != '\0';i ++){
+        for (int j = 0; i <= strlen(input_text); i++){
+            word[i] = input_text[j];
+            if (input_text[j] == ch || input_text[j] == nu){
                 printf("%c\n", word);
                 strcpy(word, "");
                 }
@@ -20,12 +20,12 @@ void print_words(char input_text[]) {
             }
         }
 
-    }
+
 }
 
 // Driver code. You should not need to modify this.
 int main() {
-    FILE *file_pointer = fopen("task1.txt", "r");
+    FILE *file_pointer = fopen("task2.txt", "r");
     char *input = malloc(sizeof(char) * 1000);
 
     fgets(input, 1000, file_pointer);
